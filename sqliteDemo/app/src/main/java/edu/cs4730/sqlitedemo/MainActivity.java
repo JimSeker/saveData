67b31fdf091195007bb79039c60c85ce2b6afcdd
JimSeker <seker@uwyo.edu>
2015-09-29 15:38:10 -0600

    added content provider reader example.

--- sqliteDemo/app/src/main/java/edu/cs4730/sqlitedemo/MainActivity.java
+++ sqliteDemo/app/src/main/java/edu/cs4730/sqlitedemo/MainActivity.java
@@ -9,64 +9,73 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-	String TAG = "MainActivity";
+    String TAG = "MainActivity";
 
-	SectionsPagerAdapter mSectionsPagerAdapter;
-	ViewPager mViewPager;
+    SectionsPagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the activity.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-		//mViewPager.setCurrentItem(7);// set to a specific page in the pager.
-	}
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        //mViewPager.setCurrentItem(7);// set to a specific page in the pager.
+    }
 
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-		@Override
-		public Fragment getItem(int position) {
-			switch (position) {
-			case 0: return new SqliteDemo_Fragment();
-			case 1: return new CursorAdapter_Fragment();
-            case 2: return new SpinnerFragment();
-			default: return null;
-			}
-		}
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
-		@Override
-		public int getCount() {
-			// Show X total pages.
-			return 3;
-		}
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new SqliteDemo_Fragment();
+                case 1:
+                    return new CursorAdapter_Fragment();
+                case 2:
+                    return new SpinnerFragment();
+                case 3:
+                    return new ContentProvider_Fragment();
+                default:
+                    return null;
+            }
+        }
 
-		@Override
-		public CharSequence getPageTitle(int position) {
-			switch (position) {
-			case 0:
-				return "sqlite Demo";
-			case 1:
-				return "CursorAdapter Demo";
-            case 2:
-                return "Spinner Cursor";
-			}
-			return null;
-		}
-	}	
+        @Override
+        public int getCount() {
+            // Show X total pages.
+            return 4;
+        }
 
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "sqlite Demo";
+                case 1:
+                    return "CursorAdapter Demo";
+                case 2:
+                    return "Spinner Cursor";
+                case 3:
+                    return "ContentProvider";
+
+            }
+            return null;
+        }
+    }
 
 
 }
