3fbd4ee6df6c6d33bdd9aa075bc9cbc3c2faf12f
JimSeker <seker@uwyo.edu>
2016-11-07 10:33:07 -0700

    fixed the Content Provider selection issue with null.

--- sqliteDemo/app/src/main/java/edu/cs4730/sqlitedemo/myDBContentProvider.java
+++ sqliteDemo/app/src/main/java/edu/cs4730/sqlitedemo/myDBContentProvider.java
@@ -14,7 +14,7 @@ import edu.cs4730.sqlitedemo.db.mySQLiteHelper;
 public class myDBContentProvider extends ContentProvider {
 
 	/*
-	 * used the following for a lot of information
+     * used the following for a lot of information
 	 * http://thinkandroid.wordpress.com/2010/01/13/writing-your-own-contentprovider/
 	 * 
 	 * We could use the ScoreDatabase, but it is not very generic and we need to create
@@ -29,106 +29,108 @@ public class myDBContentProvider extends ContentProvider {
 	 * If there were more tables publically accessable, then we would need more names/numbers for
 	 * urimatcher.
 	 */
-	
-	public static final String PROVIDER_NAME = "edu.cs4730.scoreprovider";
 
-	public static final Uri CONTENT_URI = 
-			Uri.parse("content://"+ PROVIDER_NAME + "/score");
+    public static final String PROVIDER_NAME = "edu.cs4730.scoreprovider";
+
+    public static final Uri CONTENT_URI =
+            Uri.parse("content://" + PROVIDER_NAME + "/score");
 
     private static final int SCORE = 1;
-    private static final int SCORE_ID = 2;   
-    
-	private static final UriMatcher uriMatcher;
-	static{
-		uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-		uriMatcher.addURI(PROVIDER_NAME, "score", SCORE);
-		uriMatcher.addURI(PROVIDER_NAME, "score/#", SCORE_ID);
-	}
+    private static final int SCORE_ID = 2;
 
-	static final String TAG = "myDBCP";
-	
-	//the database to be used in the contenProvider
-	ScoreDatabase db;
-	
+    private static final UriMatcher uriMatcher;
 
-	@Override
-	public String getType(Uri uri) {
-		switch (uriMatcher.match(uri)) {
-		// get all rows
-		case SCORE:
-			return "vnd.android.cursor.dir/vnd.cs4730.score";
-			// get a particular row
-		case SCORE_ID:
-			return "vnd.android.cursor.item/vnd.cs4730.score";
-		default:
-			throw new IllegalArgumentException("Unsupported URI: " + uri);
-		}
-	}
-	
-	@Override
-	public boolean onCreate() {
-		db = new ScoreDatabase(getContext());
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER_NAME, "score", SCORE);
+        uriMatcher.addURI(PROVIDER_NAME, "score/#", SCORE_ID);
+    }
+
+    static final String TAG = "myDBCP";
+
+    //the database to be used in the contenProvider
+    ScoreDatabase db;
+
+
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)) {
+            // get all rows
+            case SCORE:
+                return "vnd.android.cursor.dir/vnd.cs4730.score";
+            // get a particular row
+            case SCORE_ID:
+                return "vnd.android.cursor.item/vnd.cs4730.score";
+            default:
+                throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+    }
+
+    @Override
+    public boolean onCreate() {
+        db = new ScoreDatabase(getContext());
         db.open();
-		return true;
-	}
+        return true;
+    }
 
-	/*
-	 * So delete is a simple function
-	 *   Remember, contentproviders should be used a a wrapper class, so it calls into the ScoreDatabase
-	 *   to do the work.  But it does need setup the selection arg correctly.
-	 *  
-	 */
-	@Override
-	public int delete(Uri uri, String selection, String[] selectionArgs) {
+    /*
+     * So delete is a simple function
+     *   Remember, contentproviders should be used a a wrapper class, so it calls into the ScoreDatabase
+     *   to do the work.  But it does need setup the selection arg correctly.
+     *
+     */
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
 
-	        switch (uriMatcher.match(uri)) {
-	            case SCORE:
-	                break;
-	            case SCORE_ID:
-	                selection = selection + "_id = " + uri.getLastPathSegment();
-	                break;
-	            default:
-	                throw new IllegalArgumentException("Unknown URI " + uri);
-	        }
-	 
-	        int count = db.cpDelete(mySQLiteHelper.TABLE_NAME, selection, selectionArgs);
-	        getContext().getContentResolver().notifyChange(uri, null);
-	        return count;
-	}
-	/*
-	 * 	For the query method, but need to provide a couple more piece of information to the query
-	 * but are null values, plus pass onthe sortOrder on to it. 
-	 */
-	@Override
-	public Cursor query(Uri uri, String[] projection, String selection,
-			String[] selectionArgs, String sortOrder) {
+        switch (uriMatcher.match(uri)) {
+            case SCORE:
+                break;
+            case SCORE_ID:
+                selection = fixit(selection, "_id = " + uri.getLastPathSegment());
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
 
-	        switch (uriMatcher.match(uri)) {   
-	            case SCORE:
-	                break;
-	            case SCORE_ID:
-	                selection = selection + "_id = " + uri.getLastPathSegment();
-	                break;
-	            default:
-	                throw new IllegalArgumentException("Unknown URI " + uri);
-	        }
+        int count = db.cpDelete(mySQLiteHelper.TABLE_NAME, selection, selectionArgs);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
 
-	        Cursor c = db.cpQuery(mySQLiteHelper.TABLE_NAME, projection, selection, selectionArgs, sortOrder);
-            //this line is added for the loaders.  if we  changed the database, this allows a notification to be set.
-	        c.setNotificationUri(getContext().getContentResolver(), uri);
-	        return c;
-	}
+    /*
+     * 	For the query method, but need to provide a couple more piece of information to the query
+     * but are null values, plus pass onthe sortOrder on to it.
+     */
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
 
-	@Override
-	public Uri insert(Uri uri, ContentValues values) {
+        switch (uriMatcher.match(uri)) {
+            case SCORE:
+                break;
+            case SCORE_ID:
+                selection = fixit(selection, "_id = " + uri.getLastPathSegment());
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        Cursor c = db.cpQuery(mySQLiteHelper.TABLE_NAME, projection, selection, selectionArgs, sortOrder);
+        //this line is added for the loaders.  if we  changed the database, this allows a notification to be set.
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
         if (uriMatcher.match(uri) != SCORE) {  //can't insert by id number, so only generic is allowed.
             throw new IllegalArgumentException("Unknown URI " + uri);
         }
- 
+
         if (values == null) {   //basic error checking.  values is null, provider an empty one
             values = new ContentValues();  //or we could through an SQLExecption as well.
         }
- 
+
 
         long rowId = db.cpInsert(mySQLiteHelper.TABLE_NAME, values);
         if (rowId > 0) {   //add the row id to the uri and return it to the user.
@@ -137,20 +139,21 @@ public class myDBContentProvider extends ContentProvider {
             getContext().getContentResolver().notifyChange(noteUri, null);
             return noteUri;
         }
- 
+
         throw new SQLException("Failed to insert row into " + uri);
-	}
+    }
 
 
-	@Override
-	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
 
         int count;
         switch (uriMatcher.match(uri)) {
-            case SCORE:  break;
-            
-            case SCORE_ID:  
-            	selection = selection + "_id = "+uri.getLastPathSegment();
+            case SCORE:
+                break;
+
+            case SCORE_ID:
+                selection = fixit(selection, "_id = " + uri.getLastPathSegment());
                 break;
             default:
                 throw new IllegalArgumentException("Unknown URI " + uri);
@@ -159,8 +162,22 @@ public class myDBContentProvider extends ContentProvider {
         //this line is added for the loaders.  We changed the database, so notify everyone.
         getContext().getContentResolver().notifyChange(uri, null);
         return count;
-	}
-	
+    }
+
+    /*
+      * this is intended to deal with null and ANDs needed int he selection string value
+     */
+
+    private String fixit(String tofix, String item) {
+        if (tofix == null) {
+            tofix = item;
+        } else if (tofix.compareTo("") ==0) {
+            tofix = item;
+        }else {
+            tofix += " AND " + item;
+        }
+        return tofix;
+    }
 
 
 }
