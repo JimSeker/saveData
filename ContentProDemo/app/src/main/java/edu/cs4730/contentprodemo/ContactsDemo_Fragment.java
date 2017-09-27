3a36e197fd9e8638c977cc69697b78eec84ce006
Jim Ward <seker@uwyo.edu>
2017-09-27 13:48:37 -0600

    addd permission checks for access contacts.

--- ContentProDemo/app/src/main/java/edu/cs4730/contentprodemo/ContactsDemo_Fragment.java
+++ ContentProDemo/app/src/main/java/edu/cs4730/contentprodemo/ContactsDemo_Fragment.java
@@ -1,5 +1,8 @@
 package edu.cs4730.contentprodemo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.database.Cursor;
@@ -39,74 +42,95 @@ public class ContactsDemo_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.contacts_frag, container, false);
-        //get the people URI
-        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
-        //setup the information we want for the contentprovider.
-        String[] projection = new String[]{ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.HAS_PHONE_NUMBER};
-
-        //just for fun, sort return data by name, which instead of default which is _ID I think.
-        String SortOrder = ContactsContract.Contacts.DISPLAY_NAME;  //"column name, column name"  except only have one column name.
-
-        //finally make the query
-        // cursor = managedQuery(CONTENT_URI, projection, null, null, null);  //depreicated method, use one below.
-        cursor = getActivity().getContentResolver().query(CONTENT_URI, projection, null, null, SortOrder);
-
-        //this is commented out, because better using a listview, which is what the rest of the code does.
-        //	  if (c.moveToFirst()) {
-        //	 	do {
-        //	 		String str = "Id: " + c.getString(0);
-        //	 		str += "Name: " + c.getString(1);
-        //	 	} while (c.moveToNext());
-        //	 }
-
-        //setup the listview for the fragment
-
-        if (cursor == null) {
-            Log.e(TAG, "cursor is null...");
-        }
-        Log.i(TAG, "setup up listview");
         list = (ListView) myView.findViewById(R.id.listView1);
-        list.setClickable(true);
 
-        // The desired columns to be bound
-        String[] columns = new String[]{
-                ContactsContract.Contacts.DISPLAY_NAME,
-                ContactsContract.Contacts.HAS_PHONE_NUMBER
-        };
+        setupContactsList();
 
-        // the XML defined views which the data will be bound to
-        int[] to = new int[]{
-                R.id.name,
-                R.id.phone
-        };
-
-        // create the adapter using the cursor pointing to the desired data
-        //as well as the layout information
-        dataAdapter = new SimpleCursorAdapter(getActivity(),
-                R.layout.contactlist,
-                cursor,
-                columns,
-                to,
-                0);
-
-
-        // Assign adapter to ListView
-        list.setAdapter(dataAdapter);
-        //set click listener
-        list.setOnItemClickListener(new OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                // Get the cursor, positioned to the corresponding row in the result set
-                Cursor cursor = (Cursor) list.getItemAtPosition(position);
-
-                // Should really create a dialogfragment and display all the contact info here. but I'll get to that
-                // when I have time.
-                String name = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
-                Toast.makeText(myContext, name, Toast.LENGTH_SHORT).show();
-            }
-        });
         return myView;
+    }
+
+
+    /*
+      * this method is used to setup the view and make sure it has permissions as well.
+     */
+    public void setupContactsList() {
+        //first check to see if I have permissions (marshmallow) if I don't then ask, otherwise start up the demo.
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            //I'm on not explaining why, just asking for permission.
+            Log.v(TAG, "asking for permissions");
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CONTACTS},
+                    MainActivity.REQUEST_READ_CONTACTS);
+
+        } else {
+            //get the people URI
+            Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+            //setup the information we want for the contentprovider.
+            String[] projection = new String[]{ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.HAS_PHONE_NUMBER};
+
+            //just for fun, sort return data by name, which instead of default which is _ID I think.
+            String SortOrder = ContactsContract.Contacts.DISPLAY_NAME;  //"column name, column name"  except only have one column name.
+
+            //finally make the query
+            // cursor = managedQuery(CONTENT_URI, projection, null, null, null);  //depreicated method, use one below.
+            cursor = getActivity().getContentResolver().query(CONTENT_URI, projection, null, null, SortOrder);
+
+            //this is commented out, because better using a listview, which is what the rest of the code does.
+            //	  if (c.moveToFirst()) {
+            //	 	do {
+            //	 		String str = "Id: " + c.getString(0);
+            //	 		str += "Name: " + c.getString(1);
+            //	 	} while (c.moveToNext());
+            //	 }
+
+            //setup the listview for the fragment
+
+            if (cursor == null) {
+                Log.e(TAG, "cursor is null...");
+            }
+            Log.i(TAG, "setup up listview");
+
+            list.setClickable(true);
+
+            // The desired columns to be bound
+            String[] columns = new String[]{
+                    ContactsContract.Contacts.DISPLAY_NAME,
+                    ContactsContract.Contacts.HAS_PHONE_NUMBER
+            };
+
+            // the XML defined views which the data will be bound to
+            int[] to = new int[]{
+                    R.id.name,
+                    R.id.phone
+            };
+
+            // create the adapter using the cursor pointing to the desired data
+            //as well as the layout information
+            dataAdapter = new SimpleCursorAdapter(getActivity(),
+                    R.layout.contactlist,
+                    cursor,
+                    columns,
+                    to,
+                    0);
+
+
+            // Assign adapter to ListView
+            list.setAdapter(dataAdapter);
+            //set click listener
+            list.setOnItemClickListener(new OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                    // Get the cursor, positioned to the corresponding row in the result set
+                    Cursor cursor = (Cursor) list.getItemAtPosition(position);
+
+                    // Should really create a dialogfragment and display all the contact info here. but I'll get to that
+                    // when I have time.
+                    String name = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
+                    Toast.makeText(myContext, name, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
 
--- ContentProDemo/app/src/main/java/edu/cs4730/contentprodemo/MainActivity.java
+++ ContentProDemo/app/src/main/java/edu/cs4730/contentprodemo/MainActivity.java
@@ -1,24 +1,34 @@
 package edu.cs4730.contentprodemo;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     String TAG = "MainActivity";
+    public static final int REQUEST_READ_CONTACTS = 0;
 
     SectionsPagerAdapter mSectionsPagerAdapter;
     ViewPager mViewPager;
+    ContactsDemo_Fragment myContactsFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //for premissions and viewpager
+        myContactsFrag = new ContactsDemo_Fragment();
+
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -27,6 +37,34 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
         //mViewPager.setCurrentItem(7);// set to a specific page in the pager.
+    }
+
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        Log.v(TAG, "onRequest result called.");
+        switch (requestCode) {
+            case REQUEST_READ_CONTACTS:
+                //received result for GPS access
+                Log.v(TAG, "Received response for contacts permission request.");
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    Log.v(TAG, permissions[0] + " permission has now been granted. Showing preview.");
+                    Toast.makeText(this, "access to Contacts granted", Toast.LENGTH_SHORT).show();
+                    myContactsFrag.setupContactsList(); //setup the demo now.
+                } else {
+                    // permission denied,    Disable this feature or close the app.
+                    Log.v(TAG, "Contacts permission was NOT granted.");
+                    Toast.makeText(this, "Contacts access NOT granted", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     /**
@@ -45,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 case 0:
                     return new Contentp_Fragment();
                 case 1:
-                    return new ContactsDemo_Fragment();
+                    return myContactsFrag;
                 default:
                     return null;
             }
