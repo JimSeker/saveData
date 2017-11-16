025bf3ff6d2b10652169c88b89006a1c102b9b0a
Jim Ward <seker@uwyo.edu>
2017-11-16 08:38:22 -0700

    refresh of the example to better show saving data.

--- saveDataDemo/app/src/main/java/edu/cs4730/SaveData/MainActivity.java
+++ saveDataDemo/app/src/main/java/edu/cs4730/SaveData/MainActivity.java
@@ -3,84 +3,110 @@ package edu.cs4730.SaveData;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-	String d1, d3;
-	EditText t1,t2,t3;
-	TextView log;
-	
+    int b1 = 0, b2 = 0, b3 = 0;
+    EditText t1;
+    TextView logger, tv_nothing, tv_bundle, tv_preference;
+    Button button;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        t1 = (EditText)findViewById(R.id.editText1);
-        t2 = (EditText)findViewById(R.id.editText2);
-        t3 = (EditText)findViewById(R.id.editText3);
-        log = (TextView)findViewById(R.id.log);
+
+        logger = (TextView) findViewById(R.id.log);
+        t1 = (EditText) findViewById(R.id.editText1);
+
+        tv_nothing = (TextView) findViewById(R.id.tv_nothing);
+        tv_nothing.setText(String.valueOf(b1));
+
+        tv_bundle = (TextView) findViewById(R.id.tv_bundle);
         if (savedInstanceState != null) { //There is saved data
-        	logthis("There is data, restoring");
-        	d1 = savedInstanceState.getString("d1");
-        	if (d1 != null) {
-        		t1.setText(d1);
-        	}
+            logthis("There is data, restoring");
+            b2 = savedInstanceState.getInt("b2", 0);  //default in case of issues.
+            tv_bundle.setText(String.valueOf(b2));
         } else {
-        	logthis("No data in savedInstanceState");
+            logthis("No data in savedInstanceState");
         }
+
+        tv_preference = (TextView) findViewById(R.id.tv_preference);
+        //settext handled in getprefs();
+
+        button = (Button) findViewById(R.id.button);
+
+
         getprefs();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //increment each number.  over rotations and starts, these numbers will be different.
+                b1++;
+                tv_nothing.setText(String.valueOf(b1));
+                b2++;
+                tv_bundle.setText(String.valueOf(b2));
+                b3++;
+                tv_preference.setText(String.valueOf(b3));
+
+            }
+        });
     }
-    
+
     @Override
     public void onPause() {
-    	  super.onPause();
-    	  logthis("OnPause called");
-    	  // Store values between instances here
-    	  SharedPreferences preferences = getSharedPreferences("example",MODE_PRIVATE);
-    	  SharedPreferences.Editor editor = preferences.edit();
-    	  
-    	  d3 = t3.getText().toString();
-    	  editor.putString("d3",d3);
-    	  editor.commit();
-    	  logthis("Stored preferences");
+        super.onPause();
+        logthis("OnPause called");
+        // Store values between instances here
+        SharedPreferences preferences = getSharedPreferences("example", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        //store b3 in preferences
+        editor.putInt("b3", b3);
+        editor.commit();
+        logthis("Stored preferences");
     }
-    
+
     @Override
     public void onResume() {
-    	super.onResume();
-    	logthis("OnResume called");
-    	getprefs();
+        super.onResume();
+        logthis("OnResume called");
+        getprefs();
     }
-    
+
     /*
      * getpres() allows me to get the sharePreferences code in on place, it is called from 
      * onCreate and onPause.
      */
     void getprefs() {
-    	logthis("Restoring preferences.");
-    	  // Get the between instance stored values
-    	SharedPreferences preferences = getSharedPreferences("example",MODE_PRIVATE);
-    	  //get the key d3 and set a default value of "" if the key doesn't exist.  IE the first time this app is run.
-    	  d3 = preferences.getString("d3", "");
-    	  t3.setText(d3);
+        logthis("Restoring preferences.");
+        // Get the between instance stored values
+        SharedPreferences preferences = getSharedPreferences("example", MODE_PRIVATE);
+        //get the key d3 and set a default value of "" if the key doesn't exist.  IE the first time this app is run.
+        b3 = preferences.getInt("b3", 0);
+        tv_preference.setText(String.valueOf(b3));
     }
+
     /* (non-Javadoc)
      * @see android.app.Activity#onSaveInstanceState(android.os.Bundle)
      */
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
-    	//Store the state of d1/t1 and d2/t2
-    	d1 = t1.getText().toString();
-    	savedInstanceState.putString("d1", d1);
-    	super.onSaveInstanceState(savedInstanceState);
+        //Store the state et_bundle/b2
+        savedInstanceState.putInt("b2", b2);
+        super.onSaveInstanceState(savedInstanceState);
     }
-	/*
-	 * simple method to add the log TextView.
-	 */
-	public void logthis (String newinfo) {
-		if (newinfo.compareTo("")!=0) {
-			log.setText(log.getText() + "\n" + newinfo);
-		}
-	}
+
+    /*
+     * simple method to add the log TextView.
+     */
+    public void logthis(String newinfo) {
+        if (newinfo.compareTo("") != 0) {
+            logger.append(newinfo + "\n");
+        }
+    }
 }
