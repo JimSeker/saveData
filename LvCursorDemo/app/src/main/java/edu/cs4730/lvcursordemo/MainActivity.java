970d8ffce7734cabf623a42650e024baf20a8762
Jim Ward <seker@uwyo.edu>
2020-09-17 09:38:56 -0600

    fixed a display error.

--- LvCursorDemo/app/src/main/java/edu/cs4730/lvcursordemo/MainActivity.java
+++ LvCursorDemo/app/src/main/java/edu/cs4730/lvcursordemo/MainActivity.java
@@ -15,42 +15,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-	String TAG = "MainActivity";
-	BottomNavigationView bnv;
+    String TAG = "MainActivity";
+    BottomNavigationView bnv;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		bnv = findViewById(R.id.bnv);
-		bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-			@Override
-			public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-				//At this point, we are doing the same thing that is done for menu selections.
-				//if we had a onOptionsItemSelect method for a menu, we could just use it.
-				int id = item.getItemId();
-				if (id == R.id.simple) {
-					getSupportFragmentManager().beginTransaction()
-						.replace(R.id.container, new simple_Fragment()).commit();
-					return true;
-				} else if (id == R.id.custom) {
-					getSupportFragmentManager().beginTransaction()
-						.replace(R.id.container, new custom_Fragment()).commit();
-					return true;
-				} else if (id == R.id.explist) {
-					getSupportFragmentManager().beginTransaction()
-						.replace(R.id.container,  new ExpListview_Fragment()).commit();
-					return true;
-				}
-				return false;
-			}
-		});
+        bnv = findViewById(R.id.bnv);
+        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //At this point, we are doing the same thing that is done for menu selections.
+                //if we had a onOptionsItemSelect method for a menu, we could just use it.
+                int id = item.getItemId();
+                if (id == R.id.simple) {
+                    getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, new simple_Fragment()).commit();
+                    return true;
+                } else if (id == R.id.custom) {
+                    getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, new custom_Fragment()).commit();
+                    return true;
+                } else if (id == R.id.explist) {
+                    getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, new ExpListview_Fragment()).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
 
-		//set the first one as the default.
-		getSupportFragmentManager().beginTransaction()
-			.add(R.id.container, new simple_Fragment()).commit();
-
-	}
+        if (savedInstanceState == null) {
+            //set the first one as the default.
+            getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new simple_Fragment()).commit();
+        }
+    }
 
 }
