4236682ea12bf03d7e093658c1089a4bf4395ab9
Jim Ward <JimSeker@users.noreply.github.com>
2024-04-17 11:31:21 -0600

    updated api 34 and gradle.

--- saveDataDemo/app/src/main/java/edu/cs4730/SaveData/MainActivity.java
+++ saveDataDemo/app/src/main/java/edu/cs4730/SaveData/MainActivity.java
@@ -4,9 +4,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
